e4fc487e94084ffc85652b0e0f0652139ff4e181
Hyeonsoo David Lee <hyeonsoo.david.lee@gmail.com>
2021-09-28 08:42:47 +0900

    Deploy website - based on 41169bf02806de540a1aabce6a10a54c261906db

---
+++ assets/js/b0f1c099.438a34e3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcivilizeddev_github_io=self.webpackChunkcivilizeddev_github_io||[]).push([[387],{6462:function(e){e.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"/blog/tags/kubernetes","name":"kubernetes","count":1,"permalink":"/blog/tags/kubernetes"}')}}]);
